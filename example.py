d998ca9c475537363515b6bf00ea6f64aae35d5b
O-chang <yasuaki.oue@gmail.com>
2025-11-24 14:57:01 +0900

    ハッカソン事前テスト

---
+++ example.py
@@ -0,0 +1,17 @@
+print("hello world")
+def greet ():
+    print("こんにちは")
+greet()
+def print_name(name):
+    print("私の名前は{name}です")
+name = "name"
+print_name(name)
+def get_greet():
+    return "おはようございます"
+message = get_greet()
+print(message)
+def add(a,b):
+    a = 3
+    b = 5
+    return a + b
+print(add(3,5))
